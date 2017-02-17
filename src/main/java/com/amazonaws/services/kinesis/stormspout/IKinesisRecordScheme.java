6a4176b2c661f0d7e8fa371847c672676c061976
Shaun Senecal <shauns@zillowgroup.com>
2017-02-16 16:13:39 -0800

    moving to the latest stable build of Storm

--- src/main/java/com/amazonaws/services/kinesis/stormspout/IKinesisRecordScheme.java
+++ src/main/java/com/amazonaws/services/kinesis/stormspout/IKinesisRecordScheme.java
@@ -17,7 +17,7 @@ package com.amazonaws.services.kinesis.stormspout;
 
 import java.util.List;
 
-import backtype.storm.tuple.Fields;
+import org.apache.storm.tuple.Fields;
 
 import com.amazonaws.services.kinesis.model.Record;
 
--- src/main/samples/SampleTopology.java
+++ src/main/samples/SampleTopology.java
@@ -22,13 +22,13 @@ import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
-import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.InvalidTopologyException;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.regions.Regions;
